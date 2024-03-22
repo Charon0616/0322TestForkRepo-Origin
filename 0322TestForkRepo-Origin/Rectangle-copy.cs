f7e6799ed7db2682f7fdb536bc1466a6f22cb2f3
Davi Ji <v-weji1@microsoft.com>
2024-03-22 08:05:44 +0000

    Changes from Forked repo

--- 0322TestForkRepo-Origin/Rectangle-copy.cs
+++ 0322TestForkRepo-Origin/Rectangle-copy.cs
@@ -4,7 +4,7 @@ namespace RectangleApp
 {
     class Rectangle001
     {qwer
-        //定义数据类型为double型,且有两个变量，lenght和widthasdfgsdsasdfdfas
+        //定义数据类型为double型,且有两个变量，lenght和widthasdfgsdsasdfdfashhh
         double length;
         double width;//afds
         public void Accepdetails() //为类Rectagle的成员函数qwwersdftertwasdfsdfaaggggggcxvb
@@ -15,11 +15,11 @@ namespace RectangleApp
             width = 3.5;
             //width = 3.5;//new line sdqewrqYERTYFFFgsdfassdfaasdfwervzxcvadsggdsgsdfthhhhhhh
             //width = 3.5;//new line hy
-            //length = 4.5;//nsdfgdsadsgsgdcxbvfasdfsdfgsdfasasdfasdggasdfaxcvxffg
+            //length = 4.5;//nsdfgdsadsgsgdcxbvfasdfsdfgsdfasasdfasdggasdfaxcvxffghhh
         }
         public double GetArea()  //为类Rectagle的成员函数ggsgsasdfgggasdfggfasdfdfgffffhhh
         {ffffffffvvx
-            return length * width;//lengthbcvbxdgsdfgggsfdagfgdsfggggswertasdgbxcv
+            return length * width;//lengthbcvbxdgsdfgggsfdagfgdsfggggswertasdgbxcvhh
         }wertvvvv
         public void Display()   //为类Rectagle的成员函数sdsdfgasdfasdfasdfgfaasdfgabbbbvvvv
         {
